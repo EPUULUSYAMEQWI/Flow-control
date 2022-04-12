336594c8ec9a7d6a175f5436758e75e20fe65de9
EPUULUSYAMEQWI <epuulucyamekwi@gmail.com>
2022-04-12 13:21:18 -0400

    first commit

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main() {
+
+printNumber()
+
+
+
+
+}
+fun printNumber() {
+    for (a in 1..1000) {
+        if (a % 6 == 0 && a % 8 == 0) {
+            println("Amekwi")
+        } else if (a % 3 == 0) {
+            println("Epuu")
+        } else if (a % 5 == 0) {
+            println("Lucy")
+
+
+
+        } else {
+    println(a)
+}
+}
+}
+      //create a function that prints out multiples
+    //of 6 and 8 between 1 and 1000.
+    //For multiples of both 6 and 8 it should print out "Bingo!"
+
+
+
+
